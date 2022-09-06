b136a3fcc406e6692fb89bc85ff2e319645488ca
rrerick <78693116+rrerick@users.noreply.github.com>
2022-09-06 12:44:40 -0300

    Delete dirr

--- geoserver/urls.py
+++
@@ -1,9 +0,0 @@
-from django.template import Context
-from django.urls import path, re_path
-from . import views
-
-app_name='geoserver'
-urlpatterns=[
-
-    path('api/', views.RedirectRetriewView.as_view(), name='workspaces')
-]
